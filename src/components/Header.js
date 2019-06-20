58af6e8d76be940d25405efabce17b0dbb36433e
Diego Moreira <diegomptcc@hotmail.com>
2019-06-20 18:12:01 -0300

    import images in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Header.css';
 
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
+
 export default function Header() {
     return(
         <header id="main-header">
             <div className="header-content">
-                <img src="" alt="InstaClone" />
-                <img src="" alt="Enviar Publicações" />
+                <img src={logo} alt="InstaClone" />
+                <img src={camera} alt="Enviar Publicações" />
             </div>
         </header>
     );
